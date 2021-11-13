091af0e01b6e732c90f95830734ddbe612851c2d
Jacob <hahkobeh@gmail.com>
2021-11-13 14:38:54 -0700

    Starting GUI

--- src/GUI.java
+++ src/GUI.java
@@ -1,5 +1,33 @@
-
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
 
 public class GUI {
-    
+    private JFrame frame;
+
+    GUI(){
+        initializeGUI();
+
+    }
+    void initializeGUI(){
+        this.frame = new JFrame("Mental Wellness Tracker");
+        frame.setSize(600,600);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // If window is closed, stop the program
+        frame.setLocationRelativeTo(null); // Makes window open in middle of screen
+
+        JPanel panel = new JPanel();
+
+        frame.add(panel);
+
+        placeComponents(panel); // Place components on the window
+
+        frame.setVisible(true);
+
+    }
+
+    private static void placeComponents(JPanel panel) {
+    }
+
 }
+
+
