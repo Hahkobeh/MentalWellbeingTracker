85b79b37ab8ff9bcd6e5bced76469b70b3afcbbe
Rachel Renegado <https://github.com/racherene>
2021-11-13 22:21:54 -0700

    commit on GUI

--- src/GUI.java
+++ src/GUI.java
@@ -18,42 +18,63 @@ public class GUI {
     private JButton okayButton;
     private JButton notTheBestButton;
 
-    //Icons
-    private Icon affirmationIcon = new ImageIcon("images/affirmationButtonOther 1.png");
-    private Icon happy = new ImageIcon("images/happy.png");
-    private Icon mad = new ImageIcon("images/mad.png");
-    private Icon okay = new ImageIcon("images/okay.png");
-    private Icon notTheBest = new ImageIcon("images/notTheBest.png");
-    private Icon sad = new ImageIcon("images/sad.png");
-    
+    // Icons
+    private Icon affirmationIcon = new ImageIcon("../images/affirmationButtonOther 1.png");
+
+    private ImageIcon happy = new ImageIcon("../images/happy.png");
+    private ImageIcon mad = new ImageIcon("../images/mad.png");
+    private ImageIcon okay = new ImageIcon("../images/okay.png");
+    private ImageIcon notTheBest = new ImageIcon("../images/notTheBest.png");
+    private ImageIcon sad = new ImageIcon("../images/sad.png");
+
+    Image happyImg = happy.getImage(); // transform it
+    Image happyTrans = happyImg.getScaledInstance(100, 100, java.awt.Image.SCALE_SMOOTH); // scale it smoothly
+    ImageIcon newHappyIcon = new ImageIcon(happyTrans); // assign to a new ImageIcon instance
+
+    Image madImg = mad.getImage(); // transform it
+    Image madTrans = madImg.getScaledInstance(100, 100, java.awt.Image.SCALE_SMOOTH); // scale it smoothly
+    ImageIcon newMadIcon = new ImageIcon(madTrans); // assign to a new ImageIcon instance
+
+    Image okayImg = okay.getImage(); // transform it
+    Image okayTrans = okayImg.getScaledInstance(100, 100, java.awt.Image.SCALE_SMOOTH); // scale it smoothly
+    ImageIcon newOkayIcon = new ImageIcon(okayTrans); // assign to a new ImageIcon instance
+
+    Image notTheBestImg = notTheBest.getImage(); // transform it
+    Image notTheBestTrans = okayImg.getScaledInstance(100, 100, java.awt.Image.SCALE_SMOOTH); // scale it smoothly
+    ImageIcon newNotTheBestIcon = new ImageIcon(notTheBestTrans); // assign to a new ImageIcon instance
+
+    Image sadImg = sad.getImage(); // transform it
+    Image sadTrans = okayImg.getScaledInstance(100, 100, java.awt.Image.SCALE_SMOOTH); // scale it smoothly
+    ImageIcon newSadIcon = new ImageIcon(sadTrans); // assign to a new ImageIcon instance
+
     String affirmations_txt;
     Affirmations affirmations;
     DailyJournal dailyJournal;
 
-    GUI(String date){
+    GUI(String date) {
         this.date = date;
 
-        madButton = new JButton("Mad");
-        happyButton = new JButton("Happy");
-        sadButton = new JButton("sad");
-        okayButton = new JButton("Okay");
-        notTheBestButton = new JButton("not baller");
+        madButton = new JButton(newMadIcon);
+        happyButton = new JButton(newHappyIcon);
+        sadButton = new JButton(newSadIcon);
+        okayButton = new JButton(newOkayIcon);
+        notTheBestButton = new JButton(newNotTheBestIcon);
 
-        
         getAffirmation = new JButton(affirmationIcon);
         journalEntry = new JButton("Add journal entry");
         getPastData = new JButton("Read journal entry");
-        
+
         initializeGUI();
-        //initializeAffirmations();
-        //initializeDailyJournal();
+        // initializeAffirmations();
+        // initializeDailyJournal();
         frame.setVisible(true);
 
     }
-    void initializeGUI(){
+
+    void initializeGUI() {
 
         this.frame = new JFrame("Mental Wellness Tracker");
-        frame.setSize(600,600);
+        frame.setSize(600, 600);
         frame.setResizable(false);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // If window is closed, stop the program
         frame.setLocationRelativeTo(null); // Makes window open in middle of screen
@@ -61,90 +82,81 @@ public class GUI {
         JPanel panel = new JPanel();
         panel.setLayout(null);
 
-
-        
-        JLabel header = new JLabel("Today is " + date , JLabel.CENTER);
+        JLabel header = new JLabel("Today is " + date, JLabel.CENTER);
         header.setForeground(Color.BLUE);
         header.setBounds(125, 20, 350, 40);
         header.setFont(new Font("Courier", Font.PLAIN, 30));
         panel.add(header);
 
-        JLabel rating = new JLabel("How was your day?" , JLabel.CENTER);
+        JLabel rating = new JLabel("How was your day?", JLabel.CENTER);
         rating.setBounds(125, 60, 350, 40);
         rating.setFont(new Font("Courier", Font.PLAIN, 15));
         panel.add(rating);
 
-        madButton.setBounds(10, 90, 70, 30);
-        panel.add(madButton);
-        happyButton.setBounds(130, 90, 70, 30);
+        madButton.setBounds(30, 90, 100, 100);
         panel.add(happyButton);
-        sadButton.setBounds(260, 90, 70, 30);
-        panel.add(sadButton);
-        okayButton.setBounds(390, 90, 70, 30);
+        happyButton.setBounds(140, 90, 100, 100);
         panel.add(okayButton);
-        notTheBestButton.setBounds(520, 90, 70, 30);
+        sadButton.setBounds(250, 90, 100, 100);
         panel.add(notTheBestButton);
-        
+        okayButton.setBounds(360, 90, 100, 100);
+        panel.add(sadButton);
+        notTheBestButton.setBounds(470, 90, 100, 100);
+        panel.add(madButton);
+
+        JLabel mad = new JLabel("MAD", JLabel.CENTER);
+        mad.setBounds(30, 150, 100, 100);
+        mad.setFont(new Font("Courier", Font.PLAIN, 12));
+        panel.add(mad);
+
+        JLabel notTheBest = new JLabel("NOT THE BEST", JLabel.CENTER);
+        notTheBest.setBounds(140, 150, 100, 100);
+        notTheBest.setFont(new Font("Courier", Font.PLAIN, 12));
+        panel.add(notTheBest);
+
         frame.add(panel);
-/*
-        journalEntry.setBounds(150, 120, 340, 10 );
-        getAffirmation.setBounds(150, 140, 340, 10 );
-        //getPastData.setBounds(150, 150, 340, 10 );
-        frame.add(journalEntry);
-        frame.add(getAffirmation);
-        //frame.add(getPastData);
-        
-
-        placeComponents(panel); // Place components on the window
-
-        getAffirmation.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e){
-                try{
-                    affirmations_txt = affirmations.getAffirmation();
-                    JLabel affirm = new JLabel(affirmations_txt, JLabel.CENTER);
-                    frame.add(affirm);
-                }catch(Exception err){
-                    System.out.println("affirmation event listener error");
-                }
-            }
-        });
-
-        journalEntry.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e){
-                try{
-                    JTextField journalText = new JTextField(200);
-
-
-                    frame.add(journalText);
-                    
-                    
-                }catch(Exception err){
-                    System.out.println("journal_entry event listener error");
-                }
-            }
-        });
-
-        getPastData.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e){
-                try{
-                    
-                }catch(Exception err){
-                    System.out.println("get_past_data event listener error");
-                }
-            }
-        });*/
-
-
+        /*
+         * journalEntry.setBounds(150, 120, 340, 10 ); getAffirmation.setBounds(150,
+         * 140, 340, 10 ); //getPastData.setBounds(150, 150, 340, 10 );
+         * frame.add(journalEntry); frame.add(getAffirmation); //frame.add(getPastData);
+         * 
+         * 
+         * placeComponents(panel); // Place components on the window
+         * 
+         * getAffirmation.addActionListener(new ActionListener() {
+         * 
+         * @Override public void actionPerformed(ActionEvent e){ try{ affirmations_txt =
+         * affirmations.getAffirmation(); JLabel affirm = new JLabel(affirmations_txt,
+         * JLabel.CENTER); frame.add(affirm); }catch(Exception err){
+         * System.out.println("affirmation event listener error"); } } });
+         * 
+         * journalEntry.addActionListener(new ActionListener() {
+         * 
+         * @Override public void actionPerformed(ActionEvent e){ try{ JTextField
+         * journalText = new JTextField(200);
+         * 
+         * 
+         * frame.add(journalText);
+         * 
+         * 
+         * }catch(Exception err){
+         * System.out.println("journal_entry event listener error"); } } });
+         * 
+         * getPastData.addActionListener(new ActionListener() {
+         * 
+         * @Override public void actionPerformed(ActionEvent e){ try{
+         * 
+         * }catch(Exception err){
+         * System.out.println("get_past_data event listener error"); } } });
+         */
 
     }
-    void initializeAffirmations(){
+
+    void initializeAffirmations() {
         affirmations = new Affirmations();
     }
 
-    void initializeDailyJournal(){
+    void initializeDailyJournal() {
         dailyJournal = new DailyJournal(date);
     }
 
@@ -153,11 +165,9 @@ public class GUI {
         //
     }
 
-    String getAffirmation(){
+    String getAffirmation() {
         return affirmations.getAffirmation();
     }
 
+
 }
-
-
-
