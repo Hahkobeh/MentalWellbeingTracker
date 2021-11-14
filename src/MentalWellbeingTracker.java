a35067fd50e34aa81f0f0716e92a3778adba81a6
Jacob <hahkobeh@gmail.com>
2021-11-13 21:13:47 -0700

    fixing issues

--- src/MentalWellbeingTracker.java
+++ src/MentalWellbeingTracker.java
@@ -13,10 +13,12 @@ public class MentalWellbeingTracker {
     public static void main(String[] args){
 
         getDate();
-        GUI gui = new GUI(date);
-        /*DailyJournal dj = new DailyJournal(date);
+        //GUI gui = new GUI(date);
+        Affirmations aff = new Affirmations();
+        System.out.println(aff.getAffirmation());
+        DailyJournal dj = new DailyJournal(date);
         dj.addEntry("journal entry 1!");
-        dj.addEntry("HELLO THGIS IS MY JOURNAL ENTRY#2!");*/
+        dj.addEntry("HELLO THGIS IS MY JOURNAL ENTRY#2!");
         //final test
 
     }
